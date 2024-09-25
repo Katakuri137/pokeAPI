b3ba17909c0b4886b9dff459b1b903ef2a25766f
Katakuri137 <ksteam@gmx.fr>
2024-09-25 14:08:53 +0200

    Premier commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,48 @@
+let score = 0;
+let goal = "";
+
+async function displayPoke() {
+    let randomId = Math.floor(Math.random() * 898);
+    let pokemons = await fetch(`https://pokebuildapi.fr/api/v1/pokemon/${randomId}`);
+    pokemons = await pokemons.json();
+    console.log(pokemons);
+    goal = pokemons.name.toUpperCase();
+    let imgContainer = document.getElementById('displayPoke');
+    imgContainer.innerHTML = "";
+    let pokePicture = document.createElement('img');
+    pokePicture.src = pokemons.image;
+    pokePicture.classList.add('poke-image');
+    imgContainer.appendChild(pokePicture);
+};
+displayPoke();
+
+function submitChoice() {
+    let guess = document.getElementById('pokeName').value.toUpperCase();
+    if (goal === guess) {
+        document.getElementById('result').className = "text-success";
+        document.getElementById('result').innerText = "Gagné";
+        score++;
+        document.getElementById('score').innerText = "Score : " + score;
+    } else {
+        document.getElementById('result').innerText = "Perdu";
+        document.getElementById('result').className = 'text-danger';
+    }
+    displayPoke();
+}
+
+let input = document.getElementById("pokeName");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        submitChoice();
+    }
+});
+
+let submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", submitChoice);
+
+async function test() {
+    let test = await fetch('http://127.0.0.1:3000/');
+    test = await test.text();
+    console.log(test);
+}
+test();
